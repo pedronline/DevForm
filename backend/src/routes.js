918d620a77ee828058e193b09cda255f9764c3b8
Pedro Morais <pedromorais2@hotmail.com>
2020-01-15 14:44:36 +0000

    ...

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,8 @@ const DevController = require("./controllers/DevController");
 
 const routes = Router();
 
+routes.get("/", (req, res) => res.json("Hello pedro"));
+// routes.get("/devs", DevController.index);
 routes.post("/devs", DevController.store);
 
 module.exports = routes;
