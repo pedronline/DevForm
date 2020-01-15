59d00e7cc69226663fc1be669ad119afed9462e3
pedronline <pedromorais2@hotmail.com>
2020-01-15 20:34:10 +0000

    search controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
 const DevController = require("./controllers/DevController");
+const SearchController = require("./controllers/SearchController");
 
 const routes = Router();
 
 routes.get("/", (req, res) => res.json("Hello pedro"));
-// routes.get("/devs", DevController.index);
+
+routes.get("/devs", DevController.index);
 routes.post("/devs", DevController.store);
+
+routes.get("/search", SearchController.index);
+
 
 module.exports = routes;
