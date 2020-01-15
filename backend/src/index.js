204ea34f15b83ea7f32c6c500a6ee71c78ab2957
Pedro Morais <pedromorais2@hotmail.com>
2020-01-15 03:46:13 +0000

    model dev created
    model dev:
    -name
    -github_username
    -bio
    -avatar_url
    -techs
    -location: {
    type: PointSchema,
    index: "2dsphere"
    }

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const routes = require("./routes");
 const dotenv = require("dotenv").config();
+
 const mongoose = require("mongoose").connect(`${process.env.MONGO_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -9,10 +11,9 @@ const port = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.json());
+app.use(routes)
 
-app.get("/", (req, res) => {
-  return res.send("hello pedro");
-});
+
 
 app.listen(port, () => {
   console.log(`Rdy on port http://localhost:${process.env.PORT}`);
